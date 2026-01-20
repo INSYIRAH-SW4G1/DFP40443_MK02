6123e6c775ffb69d71852e2a9126c72e321b88e2
INSYIRAH-SW4G1 <18dit24f1066@student.ptss.edu.my>
2026-01-20 15:53:32 +0800

    Pengasingan Kod

--- biodata.php
+++ biodata.php
@@ -11,7 +11,9 @@ $kelas = "SW4_G1";
 
     <body>
         <h1>BIODATA SAYA</h1>
+        
         <img src="image.jpg" alt="Foto Profile">
+
         <p><b>NAMA: </b><?= $nama ?></p> 
         <p><b>NO MATRIK: </b><?= $nomatrik ?></p>
         <p><b>KELAS: </b><?= $kelas ?></p>
