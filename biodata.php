892693bda00e6fa0f206e0cd57e406fe3dcf31a6
INSYIRAH-SW4G1 <18dit24f1066@student.ptss.edu.my>
2026-01-20 10:31:56 +0800

    Pengasingan kod

---
+++ biodata.php
@@ -0,0 +1,12 @@
+<?php
+$nama = "NUR INSYIRAH BINTI MD SAFUAN";
+$nomatrik = "18DIT24F1066";
+$kelas = "SW4_G1";
+?>
+
+<h1>BIODATA SAYA</h1>
+<img src="image.jpg" alt="Foto Profile">
+<p><b>NAMA: </b><?= $nama ?></p> 
+<p><b>NO MATRIK: </b><?= $nomatrik ?></p>
+<p><b>KELAS: </b><?= $kelas ?></p>
+
--- index.php
+++ index.php
@@ -1,22 +1,13 @@
-<?php
-$nama = "NUR INSYIRAH BINTI MD SAFUAN";
-$nomatrik = "18DIT24F1066";
-$kelas = "SW4_G1";
-?>
-
 <!DOCTYPE html> 
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Profile Saya</title>
+        <link rel="stylesheet" href="style.css">
     </head>
 
-    <body>
-        <h1>Profile Saya</h1>
-        
-        <p><b>Nama: </b><?= $nama ?></p> 
-        <p><b>No Matrik: </b><?= $nomatrik ?></p>
-        <p><b>Kelas: </b><?= $kelas ?></p>
-
-        <img src="image.jpg">
+    <body>        
+        <?php include 'biodata.php'; ?>
     </body>
 </html>
