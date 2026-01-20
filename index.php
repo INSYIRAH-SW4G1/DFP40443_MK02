17a9fe7aa7d14d2bf37cf30893472619597bba09
INSYIRAH-SW4G1 <18dit24f1066@student.ptss.edu.my>
2026-01-20 09:37:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$nama = "NUR INSYIRAH BINTI MD SAFUAN";
+$nomatrik = "18DIT24F1066";
+$kelas = "SW4_G1";
+?>
+
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>Profile Saya</title>
+    </head>
+
+    <body>
+        <h1>Profile Saya</h1>
+        
+        <p><b>Nama: </b><?= $nama ?></p> 
+        <p><b>No Matrik: </b><?= $nomatrik ?></p>
+        <p><b>Kelas: </b><?= $kelas ?></p>
+
+        <img src="image.jpg">
+    </body>
+</html>
